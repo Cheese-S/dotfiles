bf097874c88815a81a90872f2a30cb1b2c975750
cheese_s <cheese_s@cheery.dev>
2024-10-27 16:13:34 -0700

    dotfiles init

---
+++ nvim/init.lua
@@ -0,0 +1,1 @@
+require("cheese_s")
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,30 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.default_prog = { 'powershell.exe' }
+
+-- font
+config.font = wezterm.font_with_fallback({
+	{
+		family = "CaskaydiaCove Nerd Font Mono"
+	},
+	{
+		family = "Cascadia Code"
+	},
+})
+
+-- looks
+config.color_scheme = 'Gruvbox Material (Gogh)'
+config.window_background_opacity = 0.8
+config.win32_system_backdrop = 'Acrylic'
+config.window_decorations = 'RESIZE'
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+return config
+
